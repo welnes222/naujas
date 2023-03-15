0bf451e40c24fa2c7df6ed3ab8e003bed2618d12
welnes222 <rokas222@hotmail.com>
2023-03-15 18:10:34 +0200

    padaryta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,100 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+//        String name = "Tomas";
+//        String surname = "Vandenspygelis";
+//
+//        if (name.length() > surname.length()){
+//            System.out.println(name);
+//        }
+//        if (surname.length() > name.length()){
+//            System.out.println(surname);
+//        }
+
+
+
+//        String name = "Tomas";
+//        String surname = "Vandenspygelis";
+//
+//        System.out.println(name.toUpperCase());
+//        System.out.println(surname.toLowerCase());
+
+
+//        String name = "Tomas";
+//        String surname = "Vandenspygelis";
+//
+//        String UL = name.substring(0,1) + surname.substring(0,1);
+//        System.out.println(UL);
+
+
+
+//          String name = "Tomas";
+//          String surname = "Vandenspygelis";
+//
+//
+//          String threelast = (name.substring( name.length() - 3)) + (surname.substring( surname.length() - 3)) ;
+//          System.out.println(threelast);
+
+
+
+//        String sentence = "An American in Paris";
+//
+//        sentence.replace("a","*").replace("A","*");
+//        System.out.println(sentence.replaceAll("[aA]","*"));
+
+
+//        String sentence = "An American in Paris";
+//        System.out.println(sentence.replaceAll("[AaĄaEeĘęĖėIiĮįYyOoUuŲųŪū]",""));
+//        String sentence2 = "Breakfast at Tiffany's";
+//        System.out.println(sentence2.replaceAll("[AaĄaEeĘęĖėIiĮįYyOoUuŲųŪū]",""));
+//        String sentence3 = "2001: A Space Odyssey";
+//        System.out.println(sentence3.replaceAll("[AaĄaEeĘęĖėIiĮįYyOoUuŲųŪū]",""));
+//        String sentence4 = "It's a Wonderful Life";
+//        System.out.println(sentence4.replaceAll("[AaĄaEeĘęĖėIiĮįYyOoUuŲųŪū]",""));
+
+
+//        String starwars = ("Star Wars: Episode "+ " ".repeat( (int) (Math.random() * 10))+((int) (Math.random() * 7)+1) + " - A New Hope");
+//        System.out.println(starwars.substring( starwars.length() -14, starwars.length() -13 ));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
